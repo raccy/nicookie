9430a29d28030d70956e72faef14af624a2b890f
らっしー <open@raccy.org>
2015-04-05 19:34:52 +0900

    保存しわすれ

--- nicookie_lin.h
+++ nicookie_lin.h
@@ -1,16 +1,16 @@
 /*
- * nicookie_osx.h
+ * nicookie_lin.h
  * Copyright (c) 2015 IGARASHI Makoto (raccy)
  * This software is released under the MIT License, see LICENSE.
  */
-#ifndef nicookie_osx_H
-#define nicookie_osx_H
+#ifndef nicookie_lin_H
+#define nicookie_lin_H
 #include <stddef.h>
 
-char *nicookie_osx(char *buf, size_t size, int browser);
-char *nicookie_osx_chrome(char *buf, size_t size);
-char *nicookie_osx_firefox(char *buf, size_t size);
-char *nicookie_osx_safari(char *buf, size_t size);
-// char *nicookie_osx_opera(char *str, size_t n);
+char *nicookie_lin(char *buf, size_t size, int browser);
+char *nicookie_lin_chrome(char *buf, size_t size);
+char *nicookie_lin_firefox(char *buf, size_t size);
+char *nicookie_lin_safari(char *buf, size_t size);
+// char *nicookie_lin_opera(char *str, size_t n);
 
-#endif // nicookie_osx_H
+#endif // nicookie_lin_H
